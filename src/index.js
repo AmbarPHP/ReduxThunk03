ec7fd70ed9dea26d1c278e9ce1aa3c4ab378cd41
Marina <2175567+AmbarPHP@users.noreply.github.com>
2022-08-23 14:56:17 -0500

    commit inicila

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import Main from "./components/main.js";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers/rootReducers";
+// import reducer from "./reducers/reducers.js";
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
+function App() {
+  return (
+    <Provider store={store}>
+      <div className="App">
+        <Main />
+      </div>
+    </Provider>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+//https://redux.js.org/introduction/examples
+
